1650cad93082fbb4309e23a085cbd63e5385577d
Gabe Arnold <gabesarn@gmail.com>
2020-11-15 23:22:22 -0800

    Testing to see if I have a naming error.

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 from flask import Flask, request, render_template
-app = Flask(__name__)
+my_app = Flask(__name__)
 
-app.config['SEND_FILE_MAX_AGE_DEFAULT'] = -1
+my_app.config['SEND_FILE_MAX_AGE_DEFAULT'] = -1
 
-@app.route('/home')
-@app.route('/')
+@my_app.route('/home')
+@my_app.route('/')
 def hello_world():
     return render_template('home.html')
 
-@app.route('/about')
+@my_app.route('/about')
 def about_me_page():
     return render_template('about.html')
 
 if __name__=='__main__':
-    app.run()
+    my_app.run()
