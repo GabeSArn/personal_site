4bb6c79fc50c8a2798b4412c3128e43e07fa7337
Gabe Arnold <gabesarn@gmail.com>
2020-12-30 20:57:27 -0800

    Adjusting app.

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from flask import Flask, request, render_template
 import os
+
 my_app = Flask(__name__)
 
-@my_app.route('/home')
+
 @my_app.route('/')
 def hello_world():
     return render_template('home.html')
@@ -13,4 +14,4 @@ def about_me_page():
 
 if __name__=='__main__':
     Port = int(os.environ.get('PORT', 17995))
-    my_app.run(debug = True, port=Port)
+    my_app.run(port=Port)
