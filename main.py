5df03a7defa8d27b1a235e02eda91cdf69214608
Gabe Arnold <gabesarn@gmail.com>
2020-12-30 21:15:01 -0800

    main.py update.

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, render_template
 import os
 
-my_app = Flask(__name__)
+app = Flask(__name__)
 
 
 @my_app.route('/')
@@ -14,4 +14,4 @@ def about_me_page():
 
 if __name__=='__main__':
     Port = int(os.environ.get('PORT', 17995))
-    my_app.run(port=Port)
+    app.run(port=Port)
