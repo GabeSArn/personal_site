7ebdd8289e86dfac0aea8e2d77ba1fe7f74fc6d8
Gabe Arnold <gabesarn@gmail.com>
2021-04-03 18:57:01 -0700

    Updating button styles. Adding Form Page.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, render_template
+from forms import ContactForm
 import os
 
 app = Flask(__name__)
@@ -7,9 +8,14 @@ app = Flask(__name__)
 def hello_world():
     return render_template('home.html')
 
-@app.route('/about')
+@app.route('/about', methods=['GET', 'POST'])
 def about_me_page():
-    return render_template('about.html')
+    form = ContractForm()
+
+    if request.method == 'POST':
+        return 'Form Submitted.'
+    elif request.method == 'GET':
+        return render_template('about.html', form=form)
 
 if __name__=='__main__':
     Port = int(os.environ.get('PORT', 17995))
