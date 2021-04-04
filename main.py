b37ba31b601c43bf7b8700915734526e90b13a34
Gabe Arnold <gabesarn@gmail.com>
2021-04-04 09:56:59 -0700

    Heroku fix.

--- main.py
+++ main.py
@@ -10,12 +10,12 @@ def hello_world():
 
 @app.route('/about', methods=['GET', 'POST'])
 def about_me_page():
-    form = ContractForm()
-
-    if request.method == 'POST':
-        return 'Form Submitted.'
-    elif request.method == 'GET':
-        return render_template('about.html', form=form)
+    # form = ContractForm()
+    #
+    # if request.method == 'POST':
+    #     return 'Form Submitted.'
+    # elif request.method == 'GET':
+    return render_template('about.html', form=form)
 
 if __name__=='__main__':
     Port = int(os.environ.get('PORT', 17995))
